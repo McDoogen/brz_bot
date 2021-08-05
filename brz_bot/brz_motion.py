3b6901656d1c61fe71bf3db38cc8e3e88ae7abf6
Douglas Smith <McDoogen@gmail.com>
2021-08-05 17:14:55 -0400

    import Twist messages

--- brz_bot/brz_motion.py
+++ brz_bot/brz_motion.py
@@ -1,19 +1,20 @@
 import rclpy
 from rclpy.node import Node 
 
-from std_msgs.msg import String # Will need to change to cmd_vel ?
+from geometry_msgs.msg import Twist
 
 class BrzMotion(Node):
     def __init__(self):
         super().__init__('brz_motion')
         self.subscription = self.create_subscription(
-            String,
-            'topic',
+            Twist,
+            'key_vel',
             self.topic_callback,
             10)
     
     def topic_callback(self, msg):
         self.get_logger().info(" I hear you calling!")
+        self.get_logger().info(str(msg.linear.x))
 
 def main(args=None):
     rclpy.init(args=args)
