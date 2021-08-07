ddaf78f6bebd885a7d7e223f1ea3208b9387e509
Douglas Smith <McDoogen@gmail.com>
2021-08-07 10:58:00 -0400

    linear/angular printout

---
+++ README.md
@@ -0,0 +1,18 @@
+# brz-bot
+brz-bot is an experimentation with ROS 2, localization, and teleoperation! Fun! :D
+
+# Parts Needed for Phase 2:
+
+
+# Parts needed for Future Phases:
+1. Battery Pack for Logic (5V)
+2. Battery Pack for Motor (5V)
+3. Improved Motor Battery connections (find JST...)
+4. Improved Mount for Pi to Robot Base
+5. Dynamixel Motors
+    -> This will affect voltage needs as well
+    -> This will need more space potentially meaning modifications to the robot base
+    -> Check compatibility with Raspberry PI.
+6. Ultrasonic Range Sensor Integration
+
+Batteries can be 9V for a smaller footprint and then stepped down. What is the Voltage range for the PI?
--- brz_bot/brz_motion.py
+++ brz_bot/brz_motion.py
@@ -1,20 +1,28 @@
+from adafruit_motorkit import MotorKit
+
+# ROS Imports
 import rclpy
 from rclpy.node import Node 
-
 from geometry_msgs.msg import Twist
+
 
 class BrzMotion(Node):
     def __init__(self):
         super().__init__('brz_motion')
+        self.kit = MotorKit()
         self.subscription = self.create_subscription(
             Twist,
             'key_vel',
             self.topic_callback,
             10)
+        
     
     def topic_callback(self, msg):
-        self.get_logger().info(" I hear you calling!")
-        self.get_logger().info(str(msg.linear.x))
+        # TODO: Here we want to react to the velocity commands received from the key_vel topic
+        # We'll receive Lienar and Angular speeds, we in turn set the motors to move accordingly. Is there already a library for this?
+        print_out = f"Linear X: {msg.linear.x}, Y: {msg.linear.y}, Z: {msg.linear.z}\n"
+        print_out+= f"Angular X: {msg.angular.x}, Y: {msg.angular.y}, Z: {msg.angular.z}"
+        self.get_logger().info(print_out)
 
 def main(args=None):
     rclpy.init(args=args)
